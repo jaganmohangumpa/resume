1720376b400503a5b95e5c35a055902146cf8ffa
Jagan Mohan G <jaganmohangumpa@gmail.com>
2018-02-06 00:21:56 +0530

    created basicsCard(data) & address(location) to render profile basics

--- scripts/main.js
+++ scripts/main.js
@@ -16,6 +16,7 @@
   loadJSON("resources/data.json", function(text){
     let data = JSON.parse(text);
     console.log(data);
+    basicsCard(data.basics);
   });
 
   var app = {
@@ -25,5 +26,35 @@
    },
    profileCard: document.querySelector('.profile-card')
  };
+
+ function basicsCard(data){
+      //app.profileCard.querySelector('.name').textContent = data.name;
+      var card = app.profileCard;
+      card.querySelector('.name').textContent = data.name;
+      card.querySelector('.label').textContent = data.label;
+      let mobile = card.querySelector('.mobile .info .value');
+      mobile.textContent = data.phone;
+      mobile.href = "tel:" + data.phone;
+      let email = card.querySelector('.email .info .value');
+      email.textContent = data.email;
+      email.href = "mailto:" + data.email;
+      let link = "";
+      for(var i in data.profiles){
+        link += "<a href='" + data.profiles[i].url + "' ><img src='" + app.icon[data.profiles[i].network] + "' /></a>";
+      }
+      card.querySelector('.social-links').innerHTML = link;
+
+      card.querySelector('.location .info').textContent = address(data.location);
+  }
+
+  function address(location){
+    let loc = [];
+    for(var key in location){
+      if(location.hasOwnProperty(key)) {
+      loc.push(location[key]);
+     }
+    }
+    return loc.join(", ");
+  }
 
 })();
