1e0af05c228506886457772df4df4d814ac5902e
Jagan Mohan G <jaganmohangumpa@gmail.com>
2018-02-06 00:06:47 +0530

    created JS function loadJSON(file, callback)

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,15 @@
 (function() {
-  
+
+  function loadJSON(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+      if (rawFile.readyState === 4 && rawFile.status == "200") {
+        callback(rawFile.responseText);
+      }
+    };
+    rawFile.send(null);
+  }
+
 })();
