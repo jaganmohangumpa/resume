faad13032851308263b189ced4a4d4d48cc774fb
jaganmohangumpa <jaganmohangumpa@gmail.com>
2019-09-17 19:59:34 +0530

    update sw.js

--- sw.js
+++ sw.js
@@ -20,9 +20,27 @@ function onFetch(e) {
   const url = new URL(e.request.url);
   console.log(url);
 }
+
+function onPush(e) {
+    console.log('[Service Worker] Push Received.');
+    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+}
+
+fucntion onNotificationclick(e) {
+    console.log('[Service Worker] Notification click Received.');
+
+    event.notification.close();
+
+    event.waitUntil(
+        clients.openWindow('https://developers.google.com/web/')
+    );
+}
+
 var CACHE_VERSION = "v1.0.0",
     CACHE_NAME = CACHE_VERSION + ":sw-cache::",
     REQUESTS_LIMIT = 70;
 self.addEventListener("install", onInstall), 
 self.addEventListener("activate", onActivate), 
-self.addEventListener("fetch", onFetch);
+self.addEventListener("fetch", onFetch),
+self.addEventListener("push", onPush),
+self.addEventListener('notificationclick', onNotificationclick);
